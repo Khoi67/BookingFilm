a9381befbb277d385b97b21d118eb4e9b4984c64
Khoi67 <khoi672001@gmail.com>
2023-04-01 21:35:43 +0700

    ''

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BookingMovie  from './movieBooking'
+
+function App() {
+  return (
+    <div className="App">
+      <BookingMovie />
+    </div>
+  );
+}
+
+export default App;
